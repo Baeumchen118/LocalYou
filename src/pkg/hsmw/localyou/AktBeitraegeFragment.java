e1a91007bb0a4b2e0234d9f3d92df4cad411f888
Thomas Kleebaum <tkleebau@hs-mittweida.de>
2014-09-28 11:24:53 +0200

    rename pkg directory

---
+++ src/pkg/hsmw/localyou/AktBeitraegeFragment.java
@@ -0,0 +1,20 @@
+package edu.hsmw.localyou;
+
+import edu.hsmw.localyou.R;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AktBeitraegeFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View rootView = inflater.inflate(R.layout.fragment_aktbeitraege, container, false);
+		
+		return rootView;
+	}
+}
